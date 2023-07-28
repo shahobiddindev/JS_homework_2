7c9eb998e0e44aa824e0dd46bfb99ac41f91b1e5
shahobiddindev <xalilovreactjs03@gmail.com>
2023-07-28 10:46:08 +0500

    first commit

---
+++ js/mantiqiy_operator.js
@@ -0,0 +1,294 @@
+// Boolean1
+
+// let A = +prompt("Son kiriting:") ;
+
+// let x = (A % 2 == 0) ;
+
+// console.log(x);
+
+// Boolean2
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let res = A >= 0 || B <-2 ;
+
+// console.log(res);
+
+// Boolean3.
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let res = A < B && B < C ;
+
+// console.log(res);
+
+// Boolean4
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let res = (A % 2 == 1) && (B % 2 == 1) ;
+
+// console.log(res);
+
+// Boolean5
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let res = (A % 2 == 1) || (B % 2 == 1) ;
+
+// console.log(res);
+
+// Boolean6
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let res = (A + B) % 2 == 1 ;
+
+// console.log(res);
+
+// Boolean7
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let res = A > 0 && B > 0 && C > 0 ;
+
+// console.log(res);
+
+// Boolean8
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let res = (A > 0 && B <= 0 && C <= 0) || (A <= 0 && B > 0 && C <= 0) || (A <= 0 && B <= 0 && C > 0) ;
+
+// console.log(res);
+
+// Boolean9
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let res = (A > 0 && B > 0 && C <= 0) || (A > 0 && B <= 0 && C > 0) || (A <= 0 && B > 0 && C > 0) ;
+
+// console.log(res);
+
+// Boolean10
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let res = (A / 10 >= 1) && (A / 100 < 1) && (A % 2 == 0) ;
+
+// console.log(res);
+
+// Boolean11
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let res = (A / 100 >= 1) && (A / 1000 < 1) && (A % 2 == 1) ;
+
+// console.log(res);
+
+// Boolean12
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let res = (A - B == 0 || A - C == 0 || B - C == 0 ) ;
+
+// console.log(res);
+
+// Boolean13
+
+// let abc = +prompt(" 3 xonali son kiriting:") ;
+
+// let c = abc % 10 ;
+
+// let b = ((abc % 100) - c) / 10 ;
+
+// let a = (abc - 10 * b - c) / 100 ;
+
+// let res = a < b && b < c ;
+
+// console.log(res);
+
+// Boolean14
+
+// let abc = +prompt(" 3 xonali son kiriting:") ;
+
+// let c = abc % 10 ;
+
+// let b = ((abc % 100) - c) / 10 ;
+
+// let a = (abc - 10 * b - c) / 100 ;
+
+// let res = (a < b && b < c) || (a > b && b > c) ;
+
+// console.log(res);
+
+// Boolean15
+
+// let abc = +prompt(" 3 xonali son kiriting:") ;
+
+// let c = abc % 10 ;
+
+// let b = ((abc % 100) - c) / 10 ;
+
+// let a = (abc - 10 * b - c) / 100 ;
+
+// let res = (a == c) ;
+
+// console.log(res);
+
+// Ifoda1
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let Y = (-B / 2) * (A**2 + B**2) ;
+
+// console.log(Y);
+
+// Ifoda2
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let Y = ((A + B) / (C + D))**2 + Math.pow(B , 2) ;
+
+// console.log(Y);
+
+// Ifoda3
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let Y = 2 * (Math.pow(A , D) + 4 * C**2 / 3) ;
+
+// console.log(Y);
+
+// Ifoda4
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let Y = 1 / A**2 * Math.pow((B / 10) , 3) * (C + D)**2 ;
+
+// console.log(Y);
+
+// Ifoda5
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let Y = ((A / B - 1) / (C / (D -1)))**2 ;
+
+// console.log(Y);
+
+// Ifoda6
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let Y = ((A + B) / (C + ( D / (A + C)))) ;
+
+// console.log(Y);
+
+// Ifoda7
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let Y = ((A + B + C)**2 - (A - B - C)**2)**(1/2) ;
+
+// console.log(Y);
+
+// Ifoda8
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let Y = (A**2 + B**2 + C**2) / (A*B*C) ;
+
+// console.log(Y);
+
+// Ifoda9
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let D = +prompt("D Son kiriting:") ;
+
+// let Y = A * D**3 / 3 + B * C**2 / 2 ;
+
+// console.log(Y);
+
+// Ifoda10
+
+// let A = +prompt("A Son kiriting:") ;
+
+// let B = +prompt("B Son kiriting:") ;
+
+// let C = +prompt("C Son kiriting:") ;
+
+// let Y = Math.abs(A/B - B/C) + (A +B)**(1/4);
+
+// console.log(Y);
+
